eacfb16d609282aa7713b94b8d905cf8bccf3fed
Manish9440 <manishk94402gmail.com>
2023-09-04 18:54:48 +0530

    First Commit

---
+++ Demoproject/src/in/inueron/main/DemoApp.java
@@ -0,0 +1,12 @@
+package in.inueron.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		int a=2, b=5, sum;
+		sum=a+b;
+		System.out.println("sum of two no:"+sum);
+
+	}
+
+}
